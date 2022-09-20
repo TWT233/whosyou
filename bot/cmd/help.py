09f66ae27c92f1aa52660ea2b4e0f5d6c2c88c48
TWT <TWT2333@outlook.com>
2022-09-20 22:23:41 +0800

    cmd: help prompt refine

--- bot/cmd/help.py
+++ bot/cmd/help.py
@@ -1,22 +1,46 @@
-from khl import Message, Bot, ChannelPrivacyTypes
-from khl.card import CardMessage, Card, Module
+from khl import Message, Bot, PublicMessage, Event, ChannelPrivacyTypes
+from khl.card import CardMessage, Card, Module, Element
 from khl.command import Command, Rule
 
 
-def man(bot: Bot):
+@Command.command(aliases=['man', '?', 'help'])
+async def man_cmd(m: Message):
+    if isinstance(m, PublicMessage):
+        return await m.reply(CardMessage(build_index(), _build_temp_context()), True, True)
+    return await m.reply(CardMessage(build_index()))
+
+
+def man_tag(bot: Bot):
     @Command.command(regex=r'^\s*\(met\)\d+\(met\)\s*$', rules=[Rule.is_bot_mentioned(bot)])
-    async def man_dec(m: Message):
-        if m.channel_type is ChannelPrivacyTypes.GROUP:
-            return await m.reply(build_help())
-        return await m.reply(build_help())
+    async def dec(m: Message):
+        return await man_cmd.handler(m)
 
-    return man_dec
+    @dec.on_exception(Exception)
+    async def _(*_):
+        ...
+
+    return dec
 
 
-def build_help():
-    result = CardMessage()
-    result.append(Card(Module.Header('指令手册')))
-    result.append(Card(
+def _build_temp_context() -> Card:
+    return Card(Module.Context('该消息仅你可见'), color='#595959')
+
+
+def build_index():
+    result = Card(
+        Module.Section('操作手册（点击按钮跳转到对应页）：'),
+        Module.ActionGroup(
+            Element.Button('绑ID', 'whosyou/man_bind'),
+            Element.Button('查ID', 'whosyou/man_who'),
+            Element.Button('支持的平台/游戏', 'whosyou/man_pf'),
+        ),
+        color='#21d8a4'
+    )
+    return result
+
+
+def build_bind():
+    result = Card(
         Module.Header('绑定游戏 ID / 好友代码 / 平台用户名'),
         Module.Context('以下指令功能相同'),
         Module.Divider(),
@@ -24,9 +48,14 @@ def build_help():
         Module.Context('例：/bind 123444555 steam'),
         Module.Divider(),
         Module.Section('{平台名}id {id/代码/用户名}'),
-        Module.Context('例：steamid 123444555\n例：eaid twt233')
-    ))
-    result.append(Card(
+        Module.Context('例：steamid 123444555\n例：eaid twt233'),
+        color='#cf8888',
+    )
+    return result
+
+
+def build_who():
+    result = Card(
         Module.Header('查询我的游戏 ID / 好友代码 / 平台用户名'),
         Module.Context('以下指令功能相同'),
         Module.Divider(),
@@ -37,9 +66,14 @@ def build_help():
         Module.Context(
             '例：steamid\n(机器人会回复你：123444555)\n'
             '例：eaid\n(机器人会回复你：twt233)\n'
-        )
-    ))
-    result.append(Card(
+        ),
+        color='#8255c2'
+    )
+    return result
+
+
+def build_pf():
+    result = Card(
         Module.Header('支持的平台/游戏'),
         Module.Divider(),
         Module.Section('steam'),
@@ -51,6 +85,30 @@ def build_help():
         Module.Section('Apex'),
         Module.Context('可用别名：ap apex APEX'),
         Module.Divider(),
-    ))
-
+        color='#88bdcf'
+    )
     return result
+
+
+_man_map = {
+    'whosyou/man_bind': build_bind,
+    'whosyou/man_who': build_who,
+    'whosyou/man_pf': build_pf,
+}
+
+
+async def on_man_clicked(b: Bot, e: Event):
+    if e.body['value'] not in _man_map:
+        return
+
+    builder = _man_map[e.body['value']]
+
+    if e.body['channel_type'] == ChannelPrivacyTypes.GROUP.value:
+        ch = await b.client.fetch_public_channel(e.body['target_id'])
+        return await b.client.send(ch,
+                                   CardMessage(build_index(), builder(), _build_temp_context(), ),
+                                   temp_target_id=e.body['user_id'],
+                                   )
+    return (await b.client.fetch_user(e.body['user_id'])).send(
+        CardMessage(builder(), build_index(), _build_temp_context()),
+    )
